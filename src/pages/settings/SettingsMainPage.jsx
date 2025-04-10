7f3237ffd71a9f211a7bb1db74473ca6ac46ffc5
hyese95 <hyese3085@gmail.com>
2025-04-10 17:00:43 +0900

    링크

--- src/pages/settings/SettingsMainPage.jsx
+++ src/pages/settings/SettingsMainPage.jsx
@@ -5,6 +5,14 @@ export default function SettingsMainPage() {
         <>
             <h1>설정 메인페이지</h1>
             <Link to={"support"}>고객센터</Link>
+            <br/>
+            <Link to={"notifications"}>알림설정</Link>
+            <br/>
+            <Link to={"my-info"}>내정보</Link>
+            <br/>
+            <Link to={"privacy-policy"}>개인정보취급방침</Link>
+            <br/>
+            <Link to={"terms"}>이용약관</Link>
         </>
     )
 
--- src/pages/settings/notifications/SettingsNotificationsPage.jsx
+++ src/pages/settings/notifications/SettingsNotificationsPage.jsx
@@ -1,7 +1,11 @@
+import {Link} from "react-router-dom";
 export default function SettingsNotificationsPage(){
     return(
         <>
             <h1>알림설정</h1>
+            <Link to={"volatility"}>시세변동알림목록</Link>
+            <br/>
+            <Link to={"target-price"}>지정가변동알림목록</Link>
         </>
     )
 }
