d17b1514f015e505886c41e9a907fe5274f749c9
realsydrid <realsydrid@gmail.com>
2025-04-10 17:42:48 +0900

    거래소메인페이지

--- src/pages/settings/support/faq/SettingsSupportFaqMainPage.jsx
+++ src/pages/settings/support/faq/SettingsSupportFaqMainPage.jsx
@@ -1,6 +1,16 @@
 import {Link} from "react-router-dom";
+import {useQuery} from "@tanstack/react-query";
 
 export default function SettingsSupportFaqMainPage() {
+    // const {data:faqs}=useQuery({
+    //     queryKey: "faqs",
+    //     staleTime: 1000 * 60 *5,
+    //     cacheTime: 1000 * 60 * 10,
+    //     retry: 1
+    //     queryFn:async ()=>{
+    //         const URL= "a"
+    //     }
+    // })
     return (
         <>
             <h1>자주묻는 질문</h1>
--- src/pages/trade/TradeMainPage.jsx
+++ src/pages/trade/TradeMainPage.jsx
@@ -1,7 +1,64 @@
+import {useQuery} from "@tanstack/react-query";
+
 export default function TradeMainPage() {
+    const {data: markets, isLoading, error} = useQuery({
+        queryKey: ["markets"],
+        staleTime: 1000 * 60 * 5,
+        cacheTime: 1000 * 60 * 10,
+        retry: 1,
+        refetchInterval:500,
+        queryFn: async () => {
+            const URL = "https://api.upbit.com/v1/ticker/all?quote_currencies=KRW";
+            try {
+                await new Promise(resolve => setTimeout(resolve, 500));
+                const res = await fetch(URL);
+                if (!res.ok) throw new Error(res.status + "");
+                return res.json();
+
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    });
     return (
+
         <>
-            <h1>거래소 메인페이지</h1>
+            <h1>거래소메인페이지</h1>
+            {isLoading && <Loading/>}
+            {error && <ErrorComponent msg={error.message}/>}
+            <table>
+                <thead>
+                <tr>
+                    <th>자산명</th>
+                    <th>현재가</th>
+                    <th>변동</th>
+                    <th>거래금액</th>
+                </tr>
+                </thead>
+                <tbody>
+                {markets && markets.map((m) =>
+                    <tr key={m.market}>
+                        <td>{m.market}</td>
+                        <td>{m.trade_price}</td>
+                        <td>
+                            <p>{m.change_rate}</p>
+                            <p>{m.change_price}</p>
+                        </td>
+                        <td>{m.acc_trade_price_24h}</td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
+
+
         </>
     );
+}
+
+function Loading() {
+    return <p>Loading...</p>;
+}
+
+function ErrorComponent({msg}) {
+    return <p style={{color: "red"}}>{msg}</p>;
 }
