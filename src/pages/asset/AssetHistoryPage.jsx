2eab407e5b44c259fe66058361c815c3e98f9558
jinyoung <djawls111@gmail.com>
2025-04-20 23:52:37 +0900

    수정

--- src/pages/asset/AssetHistoryPage.jsx
+++ src/pages/asset/AssetHistoryPage.jsx
@@ -9,7 +9,7 @@ export default function LoadAssetHistory() {
     const { data: coinTransaction, isLoading: isLoading1, isError: isError1 } = useQuery({
         queryKey: ["coinTransaction"],
         queryFn: async () => {
-            const res = await fetch("http://localhost:8801/asset/coinTransactions");
+            const res = await fetch("http://localhost:8801/api/asset/coinTransactions.do");
             if (!res.ok) throw new Error("거래 내역 불러오기 실패");
             return res.json();
         }
