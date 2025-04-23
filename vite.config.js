ee80b819a1b9e6ea2578a6e901692b98f8f9c3a2
Arkite124 <ghkddudwnd@naver.com>
2025-04-24 05:35:50 +0900

    delete

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // 프론트 서버 포트
+    proxy: {
+      '/api': {
+        target: 'http://192.168.45.1:8801', // 백엔드 서버 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
