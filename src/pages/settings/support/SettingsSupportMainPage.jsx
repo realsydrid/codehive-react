93d9fe2df7e46da454239e72d2df0b09d56505e4
unknown <7735386@naver.com>
2025-04-28 17:58:46 +0900

    css , 알람 api연결

--- src/pages/settings/support/SettingsSupportMainPage.jsx
+++ src/pages/settings/support/SettingsSupportMainPage.jsx
@@ -1,16 +1,28 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import './SettingsSupportMainPage.css';
 
 export default function SettingsSupportMainPage() {
     return (
-        <>
-            <h1>고객센터 메인 페이지</h1>
-            <Link to={"notice"}>공지사항</Link>
-            <br/>
-            <Link to={"faq"}>자주묻는질문</Link>
-            <br/>
-            <Link to={"qna/write"}>1:1 문의하기</Link>
-            <br/>
-            <Link to={"qna/history"}>문의내역</Link>
-        </>
+        <div className="support-container">
+            <div className="support-title">고객 센터</div>
+            <div className="support-grid">
+                <Link to="notice" className="support-card">
+                    공지사항 및 가이드
+                    <span>거래소 이용 관련 가이드</span>
+                </Link>
+                <Link to="faq" className="support-card">
+                    자주하는 질문
+                    <span>거래소 이용 관련 자주하는 질문</span>
+                </Link>
+                <Link to="qna/write" className="support-card">
+                    1:1 문의하기
+                    <span>고객센터 24시간 운영</span>
+                </Link>
+                <Link to="qna/history" className="support-card">
+                    문의내역
+                    <span>1:1 문의내역 확인</span>
+                </Link>
+            </div>
+        </div>
     );
 }
