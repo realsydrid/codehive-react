e41400ab4b61cdddd730afa5507d0d8f95ba18e3
jinyoung <djawls111@gmail.com>
2025-05-02 23:33:41 +0900

    보유코인 클릭시 거래 페이지 이동

--- src/pages/asset/AssetNavBar.jsx
+++ src/pages/asset/AssetNavBar.jsx
@@ -1,15 +1,25 @@
-import {Link} from "react-router-dom";
+import {Nav} from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function AssetNavBar(){
     return (
-        <nav id="assetNavBar">
-            <div>
-                <ul>
-                    <li><Link to="/asset/my-asset">보유자산</Link></li>
-                    <li><Link to="/asset/history">거래내역</Link></li>
-                    <li><Link to="/asset/pending-orders">미체결</Link></li>
-                </ul>
-            </div>
-        </nav>
+        <Navbar expand='md' bg={"dark"} variant={"dark"} style={{position:"fixed",top:"3rem"
+            ,left:0, zIndex: 1000,marginLeft:0,minWidth:"32rem",width:"100%",maxWidth:"1000rem"}}>
+            <Navbar.Toggle aria-controls="freePost"/>
+            <Navbar.Collapse id="freePost" style={{marginLeft:"1rem",marginRight:"1rem"}} >
+                <Nav
+                    style={{ height:'auto' ,maxHeight: '200px',minWidth:"32rem",width:"95%",maxWidth:"100rem"
+                        ,display:"flex"
+                        ,justifyContent:"space-between", alignItems:"center",fontSize:"1rem",overflow:"hidden",
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap"}}
+                    navbarScroll
+                >
+                    <Nav.Link href="/asset/my-asset" style={{color:"white",margin:"0.5rem 2rem 0.2rem 2rem"}}>보유자산</Nav.Link>
+                    <Nav.Link href="/asset/history" style={{color:"white",margin:"0.5rem 2rem 0.2rem 2rem"}}>거래내역</Nav.Link>
+                    <Nav.Link href="/asset/pending-orders" style={{color:"white",margin:"0.5rem 2rem 0.2rem 2rem"}}>미체결</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
