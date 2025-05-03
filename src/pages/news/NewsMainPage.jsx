8b483dbbfa43a76d544c113f1cd4d48b35883e02
jinyoung <djawls111@gmail.com>
2025-05-03 18:19:05 +0900

    뉴스 메인페이지 추가

--- src/pages/news/NewsMainPage.jsx
+++ src/pages/news/NewsMainPage.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import CryptoNews from "./CryptoNews.jsx";
 import GlobalNews from "./GlobalNews.jsx";
 import ExchangeRate from "./ExchangeRate.jsx";
@@ -7,13 +7,51 @@ import "./NewsMainPage.css";
 
 export default function NewsMainPage() {
     const [activeTab, setActiveTab] = useState("main");
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        const fetchNews = async () => {
+            try {
+                const res = await fetch("http://localhost:8801/api/news/yesterday");
+                const json = await res.json();
+                setNews(json);
+            } catch (err) {
+                console.error("뉴스 로딩 실패:", err);
+            }
+        };
+
+        fetchNews();
+    }, []);
+
+    const handleNewsClick = (url) => {
+        const confirmMove = window.confirm("클릭 시 기사 본문으로 이동합니다.\n계속 진행하시겠습니까?");
+        if (confirmMove) {
+            window.open(url, "_blank");
+        }
+    };
 
     return (
         <>
             <NewsNavBar />
             <br/>
             <h1 className="news-title">뉴스 홈</h1>
-            <h2>전날 주요 뉴스</h2>
+            <h2 className="mt-5">전날 주요 뉴스</h2>
+            <div className="news-carousel-container">
+                {news.map((article, idx) => (
+                    <div className="news-card" key={idx}>
+                        <div
+                            onClick={() => handleNewsClick(article.contentUrl)}
+                            style={{ cursor: "pointer" }}
+                        >
+                            <img
+                                src={article.thumbnailUrl || "https://placehold.co/250x150?text=No+Image"}
+                                alt="썸네일"
+                            />
+                            <div className="news-card-title">{article.title}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
             <hr/>
             <div className="news-tab-container">
                 <button
