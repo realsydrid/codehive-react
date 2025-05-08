95bb9e89499b8b6ac225e3840f749dc73e7e752e
realsydrid <realsydrid@gmail.com>
2025-05-08 14:32:58 +0900

    asaa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build:{
+  //   emptyOutDir: true,
+  //   outDir:'/Users/seungyeob/IdeaProjects/codehive/src/main/resources/static',
+  // }
 
 })
