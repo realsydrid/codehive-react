4f4916e8000a1d886743388273f3e06aa5e8b61d
hyese95 <hyese3085@gmail.com>
2025-05-08 11:22:02 +0900

    배포설정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-})
+  build: {
+    emptyOutDir: true,
+    outDir: '/Users/hyese/Desktop/workspace/teamProject/BIT-HIVE-Team.codehive-/src/main/resources/static'
+  }
+});
